fa1a3c81d3b35bf2667c18a7735c9c263370998c
truffix <123494843+truffix@users.noreply.github.com>
2023-02-02 12:07:55 +0300

    Add files via upload

---
+++ exct_xml.py
@@ -0,0 +1,50 @@
+import zipfile
+import os
+import bs4
+import requests
+import pandas as pd
+
+base_dir = 'C:\\Users\TRUF\PycharmProjects\pack'  # absolute path to the data folder
+extension = ".zip"
+extension_sig = ".sig"
+
+os.chdir(base_dir)  # change directory from working dir to dir with files
+
+
+def unpack_all_in_dir(_dir):
+    for item in os.listdir(_dir):  # loop through items in dir
+        abs_path = os.path.join(_dir, item)  # absolute path of dir or file
+        if item.endswith(extension):  # check for ".zip" extension
+            file_name = os.path.abspath(abs_path)  # get full path of file
+            zip_ref = zipfile.ZipFile(file_name)  # create zipfile object
+            zip_ref.extractall(_dir)  # extract file to dir
+            zip_ref.close()  # close file
+            os.remove(file_name)  # delete zipped file
+        elif os.path.isdir(abs_path):
+            unpack_all_in_dir(abs_path)  # recurse this function with inner folder
+    for item in os.listdir(_dir):  # loop through items in dir
+        abs_path = os.path.join(_dir, item)  # absolute path of dir or file
+        if item.endswith(extension):  # check for ".zip" extension
+            file_name = os.path.abspath(abs_path)  # get full path of file
+            zip_ref = zipfile.ZipFile(file_name)  # create zipfile object
+            zip_ref.extractall(_dir)  # extract file to dir
+            zip_ref.close()  # close file
+            os.remove(file_name)  # delete zipped file
+        elif os.path.isdir(abs_path):
+            unpack_all_in_dir(abs_path)  # recurse this function with inner folder
+def del_sig (_dir):
+    for item in os.listdir(_dir):  # loop through items in dir
+        abs_path = os.path.join(_dir, item)  # absolute path of dir or file
+        if item.endswith(extension_sig):  # check for ".zip" extension
+            file_name = os.path.abspath(abs_path)  # get full path of file
+            os.remove(file_name)
+
+def exct_xml(base_dir):
+    unpack_all_in_dir(base_dir)
+    del_sig(base_dir)
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import exct_xml
+
+base_dir = 'C:\\Users\TRUF\PycharmProjects\pack'
+
+exct_xml.exct_xml(base_dir)
