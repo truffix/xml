3e1c9953a843a6e85b5cf3dd9969806d394bc879
truffix <123494843+truffix@users.noreply.github.com>
2023-02-10 09:50:47 +0300

    Add files via upload

--- xml_read.py
+++ xml_read.py
@@ -8,6 +8,7 @@ import os
 import datetime
 import re
 
+
 list_cadastr = []
 list_file_name = []
 list_create = []
@@ -17,6 +18,7 @@ list_vri = []
 list_area = []
 list_address = []
 list_cad_numb = []
+list_address_smej = []
 list_special_notes = []
 list_right_records = []
 list_right_owner = []
@@ -27,8 +29,12 @@ list_mail_address = []
 
 def read():
 
+
+
     os.chdir(r"C:\Users\User\PycharmProjects\xml\pack")
+
     now1 = datetime.datetime.now()
+    d=0
     for item in Path('.').glob('*'):
         if item.is_file():
             file = codecs.open(str(item),
@@ -44,6 +50,12 @@ def read():
             b_unique = b_unique.text
             list_cadastr.append(b_unique)
             list_file_name.append(file_name)
+        path = Path(r"C:\Users\User\PycharmProjects\xml\pack")
+        s = len(list(path.iterdir()))
+        d = d + 1
+        print(file_path, "--", d,"/", s)
+
+
 
 # собираем ОКСы
         try:
@@ -57,12 +69,25 @@ def read():
                 BS_data = BeautifulSoup(data, "xml")  # собираем кадастровые номера
 
                 b_unique = BS_data.find('cad_links')
-                b_unique = b_unique.find('cad_number')
-                b_unique = b_unique.text
-                list_oks.append(b_unique)
+                b_unique = b_unique.find_all('cad_number')
+
+                b_unique1 = []
+                for i in range(len(b_unique)):
+                    b_unique2 = b_unique[i].get_text()
+                    result = b_unique2[0]
+                    for letter in b_unique2[1:]:
+                        if letter.isupper():
+                            result += f' {letter}'
+                        else:
+                            result += letter
+                    b_unique1.append(result)
+
+#                b_unique = b_unique.text
+                list_oks.append(b_unique1)
         except:
             b_unique = " "
             list_oks.append(b_unique)
+
 
 # собираем Вид ОН
         try:
@@ -124,9 +149,17 @@ def read():
                 b_unique = b_unique.find_all('value')
                 b_unique = b_unique[-1]
                 b_unique = b_unique.getText()
-                list_area.append(b_unique)
+                result = b_unique[0]
+                for letter in b_unique[1:]:
+                    if letter.isupper():
+                        result += f' {letter}'
+                    else:
+                        result += letter
+                result = float(result)
+                list_area.append(result)
         except:
-            b_unique = " "
+            b_unique = "0"
+            b_unique = float(b_unique)
             list_area.append(b_unique)
 
 # собираем Адрес
@@ -177,6 +210,35 @@ def read():
         except:
             b_unique = " "
             list_cad_numb.append(b_unique)
+
+# собираем адреса смежников
+        try:
+            if item.is_file():
+                file = codecs.open(str(item),
+                                       "r", "utf-8")
+                file_path = os.path.abspath(str(item))
+                file_name = os.path.basename(file_path)
+                data = file.read()
+
+                BS_data = BeautifulSoup(data, "xml")
+
+                b_unique = BS_data.find('borders')
+                b_unique = b_unique.find_all('mailing_addess')
+                b_unique1 = []
+                for i in range(len(b_unique)):
+                    b_unique2 = b_unique[i].get_text()
+                    result = b_unique2[0]
+                    for letter in b_unique2[1:]:
+                        if letter.isupper():
+                            result += f' {letter}'
+                        else:
+                            result += letter
+                    b_unique1.append(result)
+
+                list_address_smej.append(b_unique1)
+        except:
+            b_unique = " "
+            list_address_smej.append(b_unique)
 
 # собираем специальные заметки
         try:
@@ -238,7 +300,8 @@ def read():
 
                 BS_data = BeautifulSoup(data, "xml")
 
-                b_unique = BS_data.find_all('right_holder')
+                b_unique = BS_data.find('right_records')
+                b_unique = b_unique.find_all('right_holders')
                 b_unique1 = []
                 for i in range(len(b_unique)):
                     b_unique2 = b_unique[i].get_text()
@@ -282,7 +345,7 @@ def read():
             list_mail_address.append(b_unique)
 
 
-    df_1 = pd.DataFrame( {'Имя файла':list_file_name,'Кадастровый номер':list_cadastr,'ОКС':list_oks, 'Вид ОН':list_vid_on, 'ВРИ':list_vri, 'Площадь':list_area, 'Адрес':list_address, 'Кад номера смежников':list_cad_numb, 'Специальные отметки':list_special_notes, 'Вид права':list_right_records, 'ФИО собственников':list_right_owner, 'Адрес правообла-я смежного ЗУ':list_mail_address})
+    df_1 = pd.DataFrame( {'Имя файла':list_file_name,'Кадастровый номер':list_cadastr,'ОКС':list_oks, 'Вид ОН':list_vid_on, 'ВРИ':list_vri, 'Площадь':list_area, 'Адрес':list_address, 'Кад номера смежников':list_cad_numb, 'Адреса смежников':list_address_smej, 'Специальные отметки':list_special_notes, 'Вид права':list_right_records, 'ФИО собственников':list_right_owner})
 #    df_1['Площадь'].astype('float')
     df_1.to_csv(r'C:\Users\User\PycharmProjects\xml\out\out.csv', encoding='utf-16')
 #    print (len(list_file_name),len(list_cadastr), len(list_oks),len(list_vid_on),len(list_vri),len(list_area))
