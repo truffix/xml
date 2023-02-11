a7d8c227a94b9b3fa3e983828bdef27a35886d23
truffix <123494843+truffix@users.noreply.github.com>
2023-02-11 21:12:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from exct_xml_oks import del_zip
+# from exct_xml_oks import del_zip
 from exct_xml_oks import del_pdf
 from exct_xml_oks import del_sig
 from exct_xml_oks import unpack_all_in_dir
-from xml_read_oks import read_oks
+# from xml_read_oks import read_oks
 import os
+import datetime
+from pathlib import Path
+import time
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import codecs
 import datetime
 
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from PIL import Image, ImageTk
+
 from tkinter import *
 from tkinter import ttk
 
@@ -32,8 +41,7 @@ class App(customtkinter.CTk):
         # self.sidebar_frame1 = customtkinter.CTkFrame(self, width=0, corner_radius=0)
         # self.sidebar_frame1.grid(row=0, column=2, rowspan=4, sticky="nsew")
         # self.sidebar_frame1.grid_rowconfigure(2, weight=0)
-
-
+        self.iconbitmap(r'C:\Users\User\PycharmProjects\xml\oks\9957d6a37c0ccdd4085cf7a739e7ca14.ico')
         #
         # scrollb = ttk.Scrollbar(self, command=self.textbox.yview)
         # scrollb.grid(row=0, column=3, sticky='nsew')
@@ -45,6 +53,7 @@ class App(customtkinter.CTk):
 
         self.textbox = customtkinter.CTkTextbox(self.scrollable_frame, width=465, height=300,activate_scrollbars="true")
         self.textbox.grid(row=0, column=0, padx=(0, 0), pady=(0, 0), sticky="nsew")
+
         # self.textbox.insert("0.0", "...Ход выполнения...\n\n\n\n")
 
 
@@ -84,7 +93,18 @@ class App(customtkinter.CTk):
         print("Путь Архивов:", self.base_dir)
         self.base_dir.replace("\"", "\\")
         os.chdir(self.base_dir)
-        self.textbox.insert("0.0","Папка с архивами:"+self.base_dir+"\n")
+        self.textbox.insert("1.0","Папка с архивами:"+self.base_dir+"\n")
+        os.chdir(self.base_dir)
+        now1 = datetime.datetime.now()
+        s = []
+        for item in Path('.').glob('*'):
+            file_path = os.path.abspath(str(item))
+            if file_path.endswith('.zip'):
+                s.append(1)
+        self.textbox.insert("0.0", "Ищу zip файлы...:\n")
+        time.sleep(0)
+        s_s = str(sum(s))
+        self.textbox.insert("0.0", "Найдено "+s_s+" zip файлов:\n")
         return self.base_dir
 
     def open_output_dialog_event_exct(self):
@@ -137,12 +157,201 @@ class App(customtkinter.CTk):
             print(del_pdf(self.base_dir))
             self.textbox.insert("0.0", del_pdf(self.base_dir) + "\n")
 
-        df = read_oks(self.base_dir)
-        # self.textbox.insert("0.0"+"\n")
-#        print (self.exct+".xlsx")
-        df.to_excel(self.exct+".xlsx")
-#         self.textbox.insert("0.0",read_oks(self.base_dir)+"!\n")
+        list_oks = []
+        list_cadastr = []
+        list_file_name = []
+        list_nazn = []
+        list_name = []
+        list_addres = []
+        extension = ".xml"
+
+
+        os.chdir(self.base_dir)
+        now1 = datetime.datetime.now()
+        poi = []
+        for item in Path('.').glob('*'):
+            file_path = os.path.abspath(str(item))
+            if file_path.endswith('.xml'):
+                poi.append(1)
+
+        d = 0
+        for item in Path('.').glob('*'):
+            file_path = os.path.abspath(str(item))
+
+            if file_path.endswith('.xml'):
+
+                if item.is_file():
+                    file = codecs.open(str(item),
+                                       "r", "utf-8")
+                    file_path = os.path.abspath(str(item))
+                    file_name = os.path.basename(file_path)
+                    data = file.read()
+
+                    BS_data = BeautifulSoup(data, "xml")  # собираем кадастровый номер
+
+                    b_unique = BS_data.find('build_record')
+                    b_unique = b_unique.find('object')
+                    b_unique = b_unique.find('cad_number')
+                    b_unique = b_unique.text
+                    list_cadastr.append(b_unique)
+                    list_file_name.append(file_name)
+
+                # собираем ОКСы
+                try:
+                    if item.is_file():
+                        file = codecs.open(str(item),
+                                           "r", "utf-8")
+                        file_path = os.path.abspath(str(item))
+                        file_name = os.path.basename(file_path)
+                        data = file.read()
+
+                        BS_data = BeautifulSoup(data, "xml")
+
+                        b_unique = BS_data.find('land_cad_numbers')
+                        b_unique = b_unique.find_all('cad_number')
+
+                        b_unique1 = []
+                        for i in range(len(b_unique)):
+                            b_unique2 = b_unique[i].get_text()
+                            result = b_unique2[0]
+                            for letter in b_unique2[1:]:
+                                if letter.isupper():
+                                    result += f' {letter}'
+                                else:
+                                    result += letter
+                            b_unique1.append(result)
+
+                        #                b_unique = b_unique.text
+                        list_oks.append(b_unique1)
+                except:
+                    b_unique = " "
+                    list_oks.append(b_unique)
+
+                # собираем Назначение
+                try:
+                    if item.is_file():
+                        file = codecs.open(str(item),
+                                           "r", "utf-8")
+                        file_path = os.path.abspath(str(item))
+                        file_name = os.path.basename(file_path)
+                        data = file.read()
+
+                        BS_data = BeautifulSoup(data, "xml")
+
+                        b_unique = BS_data.find('build_record')
+                        b_unique = b_unique.find('params')
+                        b_unique = b_unique.find_all('value')
+
+                        b_unique1 = []
+                        for i in range(len(b_unique)):
+                            b_unique2 = b_unique[i].get_text()
+                            result = b_unique2[0]
+                            for letter in b_unique2[1:]:
+                                if letter.isupper():
+                                    result += f' {letter}'
+                                else:
+                                    result += letter
+                            b_unique1.append(result)
+
+                        list_nazn.append(b_unique1)
+                except:
+                    b_unique = " "
+                    list_nazn.append(b_unique)
+
+                # собираем Наименование
+                try:
+                    if item.is_file():
+                        file = codecs.open(str(item),
+                                           "r", "utf-8")
+                        file_path = os.path.abspath(str(item))
+                        file_name = os.path.basename(file_path)
+                        data = file.read()
+
+                        BS_data = BeautifulSoup(data, "xml")  # собираем кадастровые номера
+
+                        b_unique = BS_data.find('build_record')
+                        b_unique = b_unique.find('params')
+                        b_unique = b_unique.find_all('name')
+
+                        b_unique1 = []
+                        for i in range(len(b_unique)):
+                            b_unique2 = b_unique[i].get_text()
+                            result = b_unique2[0]
+                            for letter in b_unique2[1:]:
+                                if letter.isupper():
+                                    result += f' {letter}'
+                                else:
+                                    result += letter
+                            b_unique1.append(result)
+
+                        list_name.append(b_unique1)
+                except:
+                    b_unique = " "
+                    list_name.append(b_unique)
+
+                # собираем Адрес
+                try:
+                    if item.is_file():
+                        file = codecs.open(str(item),
+                                           "r", "utf-8")
+                        file_path = os.path.abspath(str(item))
+                        file_name = os.path.basename(file_path)
+                        data = file.read()
+
+                        BS_data = BeautifulSoup(data, "xml")  # собираем кадастровые номера
+
+                        b_unique = BS_data.find('build_record')
+                        b_unique = b_unique.find('address_location')
+                        b_unique = b_unique.find_all('readable_address')
+
+                        b_unique1 = []
+                        for i in range(len(b_unique)):
+                            b_unique2 = b_unique[i].get_text()
+                            result = b_unique2[0]
+                            for letter in b_unique2[1:]:
+                                if letter.isupper():
+                                    result += f' {letter}'
+                                else:
+                                    result += letter
+                            b_unique1.append(result)
+
+                        list_addres.append(b_unique1)
+                except:
+                    b_unique = " "
+                    list_addres.append(b_unique)
+                path = Path(self.base_dir)
+                d = d + 1
+                # print(file_path, "--", d, "/", sum(s))
+                eee_poi = sum(poi)
+                eee_poi = str(eee_poi)
+                er=str(d)
+#                print(file_path+"--"+d+"/"+eee_poi)
+                self.textbox.insert("0.0", file_path+"--"+er+"/"+eee_poi+"\n")
+
+            df_1 = pd.DataFrame({'Имя файла': list_file_name, 'Кадастровый номер': list_cadastr, 'ОКС': list_oks,
+                                 'Назначение': list_nazn, 'Наименование': list_name, 'Адрес': list_addres})
+            # df_1.to_excel(r'C:\Users\User\PycharmProjects\xml\out_oks\out.xlsx', encoding='utf-16')
+        # now2 = datetime.datetime.now()
+        # now3 = now2 - now1
+        #    print(now3)
+        #    print(df_1)
+
+
+            df_1.to_excel(self.exct+".xlsx")
+
+
+
+
+
+
+
+
+
+
+
         self.textbox.insert("0.0","Готово!\n")
+
+
 if __name__ == "__main__":
     app = App()
     app.mainloop()
